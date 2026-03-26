d8c4c07fa584166c962f771a0c42d2af46cdc035
wyaxinw <a114221084@mail.shu.edu.tw>
2026-03-26 23:47:52 +0800

    115_03_19

---
+++ 115_03_19/Tutorial 7-2 - 2/Test Average/Test Average/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.VisualStyles;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Average 方法接受一個 int 陣列作為參數
+        // 並回傳該陣列中數值的平均值。
+        private double Average(int[] sArray)
+        {
+            int total = 0;
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                total += sArray[i];
+            }
+            return (double)total / sArray.Length;
+        }
+
+        // Highest 方法接受一個 int 陣列作為參數
+        // 並回傳該陣列中的最大值。
+        private int Highest(int[] sArray)
+        {
+          int highScore = sArray[0];
+            for (int i = 1; i < sArray.Length; i++)
+            {
+                if (sArray[i] > highScore)
+                {
+                    highScore = sArray[i];
+                }
+            }
+            return highScore;
+        }
+
+        // Lowest 方法接受一個 int 陣列作為參數
+        // 並回傳該陣列中的最小值。
+        private int Lowest(int[]sArray, int count)
+        {
+          int lowScore = sArray[0];
+            for (int i = 1; i < sArray.Length; i++)
+            {
+                if (sArray[i] < lowScore)
+                {
+                    lowScore = sArray[i];
+                }
+            }
+            return lowScore;
+        }
+
+        private int geFileCount()
+        {
+            int count = 0;
+            StreamReader inputFile;
+            try
+            {
+                inputFile = File.OpenText("TestScores.txt");
+                while (!inputFile.EndOfStream)
+                {
+                    inputFile.ReadLine();
+                    count++;
+                }
+                inputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                // 顯示錯誤訊息
+                MessageBox.Show(ex.Message);
+            }
+            return count;
+        }
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            //int size = geFileCount();
+            int[] scores = new int[getFilileScoreCount()];
+            StreamReader inputFile;
+            int index = 0; // declare and initialize index
+
+            try
+            {
+                inputFile = File.OpenText("TestScores.txt");
+
+                while (!inputFile.EndOfStream && index < scores.Length)
+                {
+                    scores[index]  =  int.Parse(inputFile.ReadLine());
+                    index++;
+                }
+                inputFile.Close();
+
+                testScoresListBox.Items.Add("學生人數: "+scores); 
+                foreach (int score in scores)
+                {
+
+                    testScoresListBox.Items.Add(scores[val]);
+                }
+            }
+            catch (Exception ex)
+            {
+                // 顯示錯誤訊息
+                MessageBox.Show(ex.Message);
+            }
+
+            double averageScore = Average(scores);
+            averageScoreLabel.Text = Average(scores).ToString("n1");
+            highScoreLabel.Text = Highest(scores).ToString();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
---
+++ 115_03_19/Tutorial 7-2/Test Average/Test Average/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Test_Average
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// 初始化元件方法（由設計工具產生）。
+        /// 已修改內容：
+        /// - 所有 `Text` 屬性改為繁體中文
+        /// - 所有元件字型改為字型大小 18（使用常見的繁體中文內建字型 Microsoft JhengHei）
+        /// - 適度調整元件大小與位置以配合較大的字型
+        /// 以下僅修改 UI 屬性，其他程式碼未更動。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.averageScoreDescriptionLabel = new System.Windows.Forms.Label();
+            this.lowScoreDescriptionLabel = new System.Windows.Forms.Label();
+            this.highScoreDescriptionLabel = new System.Windows.Forms.Label();
+            this.testScoresListBox = new System.Windows.Forms.ListBox();
+            this.testScoresDescriptionLabel = new System.Windows.Forms.Label();
+            this.highScoreLabel = new System.Windows.Forms.Label();
+            this.lowScoreLabel = new System.Windows.Forms.Label();
+            this.averageScoreLabel = new System.Windows.Forms.Label();
+            this.getScoresButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // averageScoreDescriptionLabel
+            // 
+            this.averageScoreDescriptionLabel.AutoSize = true;
+            this.averageScoreDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.averageScoreDescriptionLabel.Location = new System.Drawing.Point(452, 412);
+            this.averageScoreDescriptionLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.averageScoreDescriptionLabel.Name = "averageScoreDescriptionLabel";
+            this.averageScoreDescriptionLabel.Size = new System.Drawing.Size(164, 45);
+            this.averageScoreDescriptionLabel.TabIndex = 9;
+            this.averageScoreDescriptionLabel.Text = "平均分數";
+            // 
+            // lowScoreDescriptionLabel
+            // 
+            this.lowScoreDescriptionLabel.AutoSize = true;
+            this.lowScoreDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.lowScoreDescriptionLabel.Location = new System.Drawing.Point(466, 288);
+            this.lowScoreDescriptionLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lowScoreDescriptionLabel.Name = "lowScoreDescriptionLabel";
+            this.lowScoreDescriptionLabel.Size = new System.Drawing.Size(164, 45);
+            this.lowScoreDescriptionLabel.TabIndex = 8;
+            this.lowScoreDescriptionLabel.Text = "最低分數";
+            // 
+            // highScoreDescriptionLabel
+            // 
+            this.highScoreDescriptionLabel.AutoSize = true;
+            this.highScoreDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.highScoreDescriptionLabel.Location = new System.Drawing.Point(466, 140);
+            this.highScoreDescriptionLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.highScoreDescriptionLabel.Name = "highScoreDescriptionLabel";
+            this.highScoreDescriptionLabel.Size = new System.Drawing.Size(164, 45);
+            this.highScoreDescriptionLabel.TabIndex = 7;
+            this.highScoreDescriptionLabel.Text = "最高分數";
+            // 
+            // testScoresListBox
+            // 
+            this.testScoresListBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.testScoresListBox.FormattingEnabled = true;
+            this.testScoresListBox.ItemHeight = 45;
+            this.testScoresListBox.Location = new System.Drawing.Point(42, 156);
+            this.testScoresListBox.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.testScoresListBox.Name = "testScoresListBox";
+            this.testScoresListBox.Size = new System.Drawing.Size(334, 229);
+            this.testScoresListBox.TabIndex = 6;
+            // 
+            // testScoresDescriptionLabel
+            // 
+            this.testScoresDescriptionLabel.AutoSize = true;
+            this.testScoresDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.testScoresDescriptionLabel.Location = new System.Drawing.Point(42, 31);
+            this.testScoresDescriptionLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.testScoresDescriptionLabel.Name = "testScoresDescriptionLabel";
+            this.testScoresDescriptionLabel.Size = new System.Drawing.Size(164, 45);
+            this.testScoresDescriptionLabel.TabIndex = 5;
+            this.testScoresDescriptionLabel.Text = "測試分數";
+            // 
+            // highScoreLabel
+            // 
+            this.highScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.highScoreLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.highScoreLabel.Location = new System.Drawing.Point(685, 102);
+            this.highScoreLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.highScoreLabel.Name = "highScoreLabel";
+            this.highScoreLabel.Size = new System.Drawing.Size(334, 83);
+            this.highScoreLabel.TabIndex = 10;
+            this.highScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lowScoreLabel
+            // 
+            this.lowScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lowScoreLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.lowScoreLabel.Location = new System.Drawing.Point(685, 229);
+            this.lowScoreLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lowScoreLabel.Name = "lowScoreLabel";
+            this.lowScoreLabel.Size = new System.Drawing.Size(334, 104);
+            this.lowScoreLabel.TabIndex = 11;
+            this.lowScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // averageScoreLabel
+            // 
+            this.averageScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.averageScoreLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.averageScoreLabel.Location = new System.Drawing.Point(685, 380);
+            this.averageScoreLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.averageScoreLabel.Name = "averageScoreLabel";
+            this.averageScoreLabel.Size = new System.Drawing.Size(334, 108);
+            this.averageScoreLabel.TabIndex = 12;
+            this.averageScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // getScoresButton
+            // 
+            this.getScoresButton.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.getScoresButton.Location = new System.Drawing.Point(90, 526);
+            this.getScoresButton.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.getScoresButton.Name = "getScoresButton";
+            this.getScoresButton.Size = new System.Drawing.Size(389, 120);
+            this.getScoresButton.TabIndex = 13;
+            this.getScoresButton.Text = "取得分數";
+            this.getScoresButton.UseVisualStyleBackColor = true;
+            this.getScoresButton.Click += new System.EventHandler(this.getScoresButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.exitButton.Location = new System.Drawing.Point(564, 526);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(374, 120);
+            this.exitButton.TabIndex = 14;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 45F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 699);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.getScoresButton);
+            this.Controls.Add(this.averageScoreLabel);
+            this.Controls.Add(this.lowScoreLabel);
+            this.Controls.Add(this.highScoreLabel);
+            this.Controls.Add(this.averageScoreDescriptionLabel);
+            this.Controls.Add(this.lowScoreDescriptionLabel);
+            this.Controls.Add(this.highScoreDescriptionLabel);
+            this.Controls.Add(this.testScoresListBox);
+            this.Controls.Add(this.testScoresDescriptionLabel);
+            this.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.Name = "Form1";
+            this.Text = "測驗平均";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label averageScoreDescriptionLabel;
+        private System.Windows.Forms.Label lowScoreDescriptionLabel;
+        private System.Windows.Forms.Label highScoreDescriptionLabel;
+        private System.Windows.Forms.ListBox testScoresListBox;
+        private System.Windows.Forms.Label testScoresDescriptionLabel;
+        private System.Windows.Forms.Label highScoreLabel;
+        private System.Windows.Forms.Label lowScoreLabel;
+        private System.Windows.Forms.Label averageScoreLabel;
+        private System.Windows.Forms.Button getScoresButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
