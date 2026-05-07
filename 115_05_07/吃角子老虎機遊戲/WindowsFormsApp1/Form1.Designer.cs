6b43d3f081007cdbcd97bdd873e037cf2c7dae8f
wyaxinw <a114221084@mail.shu.edu.tw>
2026-05-07 15:44:17 +0800

    115_05_07

---
+++ 115_05_07/吃角子老虎機遊戲/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_depositPrompt = new System.Windows.Forms.Label();
+            this.textBox_deposit = new System.Windows.Forms.TextBox();
+            this.button_deposit = new System.Windows.Forms.Button();
+            this.label_balance = new System.Windows.Forms.Label();
+            this.label_lastWin = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label_betPrompt = new System.Windows.Forms.Label();
+            this.comboBox_bet = new System.Windows.Forms.ComboBox();
+            this.label_totalSpins = new System.Windows.Forms.Label();
+            this.label_winCount = new System.Windows.Forms.Label();
+            this.label_winRate = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_depositPrompt
+            // 
+            this.label_depositPrompt.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_depositPrompt.Location = new System.Drawing.Point(96, 43);
+            this.label_depositPrompt.Name = "label_depositPrompt";
+            this.label_depositPrompt.Size = new System.Drawing.Size(220, 40);
+            this.label_depositPrompt.TabIndex = 0;
+            this.label_depositPrompt.Text = "存入金額：$";
+            this.label_depositPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label_depositPrompt.Click += new System.EventHandler(this.label_depositPrompt_Click);
+            // 
+            // textBox_deposit
+            // 
+            this.textBox_deposit.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox_deposit.Location = new System.Drawing.Point(322, 40);
+            this.textBox_deposit.Name = "textBox_deposit";
+            this.textBox_deposit.Size = new System.Drawing.Size(200, 51);
+            this.textBox_deposit.TabIndex = 1;
+            // 
+            // button_deposit
+            // 
+            this.button_deposit.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_deposit.Location = new System.Drawing.Point(573, 43);
+            this.button_deposit.Name = "button_deposit";
+            this.button_deposit.Size = new System.Drawing.Size(131, 54);
+            this.button_deposit.TabIndex = 2;
+            this.button_deposit.Text = "存入";
+            this.button_deposit.UseVisualStyleBackColor = true;
+            // 
+            // label_balance
+            // 
+            this.label_balance.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_balance.Location = new System.Drawing.Point(96, 115);
+            this.label_balance.Name = "label_balance";
+            this.label_balance.Size = new System.Drawing.Size(300, 36);
+            this.label_balance.TabIndex = 3;
+            this.label_balance.Text = "餘額 : NT$0.00";
+            this.label_balance.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label_lastWin
+            // 
+            this.label_lastWin.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_lastWin.Location = new System.Drawing.Point(392, 97);
+            this.label_lastWin.Name = "label_lastWin";
+            this.label_lastWin.Size = new System.Drawing.Size(341, 72);
+            this.label_lastWin.TabIndex = 4;
+            this.label_lastWin.Text = "本次獲得 : NT$0.00";
+            this.label_lastWin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.Location = new System.Drawing.Point(102, 172);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 150);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox2.Location = new System.Drawing.Point(329, 172);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 150);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox3.Location = new System.Drawing.Point(552, 172);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(166, 150);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label_betPrompt
+            // 
+            this.label_betPrompt.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_betPrompt.Location = new System.Drawing.Point(126, 350);
+            this.label_betPrompt.Name = "label_betPrompt";
+            this.label_betPrompt.Size = new System.Drawing.Size(199, 53);
+            this.label_betPrompt.TabIndex = 8;
+            this.label_betPrompt.Text = "下注金額：";
+            this.label_betPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBox_bet
+            // 
+            this.comboBox_bet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_bet.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox_bet.FormattingEnabled = true;
+            this.comboBox_bet.Location = new System.Drawing.Point(365, 350);
+            this.comboBox_bet.Name = "comboBox_bet";
+            this.comboBox_bet.Size = new System.Drawing.Size(145, 44);
+            this.comboBox_bet.TabIndex = 9;
+            // 
+            // label_totalSpins
+            // 
+            this.label_totalSpins.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_totalSpins.Location = new System.Drawing.Point(109, 426);
+            this.label_totalSpins.Name = "label_totalSpins";
+            this.label_totalSpins.Size = new System.Drawing.Size(246, 59);
+            this.label_totalSpins.TabIndex = 10;
+            this.label_totalSpins.Text = "旋轉：0次";
+            this.label_totalSpins.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label_winCount
+            // 
+            this.label_winCount.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_winCount.Location = new System.Drawing.Point(323, 430);
+            this.label_winCount.Name = "label_winCount";
+            this.label_winCount.Size = new System.Drawing.Size(217, 50);
+            this.label_winCount.TabIndex = 11;
+            this.label_winCount.Text = "中獎：0次";
+            this.label_winCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label_winRate
+            // 
+            this.label_winRate.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_winRate.Location = new System.Drawing.Point(546, 413);
+            this.label_winRate.Name = "label_winRate";
+            this.label_winRate.Size = new System.Drawing.Size(253, 85);
+            this.label_winRate.TabIndex = 12;
+            this.label_winRate.Text = "勝率：0.0%";
+            this.label_winRate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(179, 507);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 66);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "旋轉";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(452, 507);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 66);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "離開";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(120, 120);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 612);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_winRate);
+            this.Controls.Add(this.label_winCount);
+            this.Controls.Add(this.label_totalSpins);
+            this.Controls.Add(this.comboBox_bet);
+            this.Controls.Add(this.label_betPrompt);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label_lastWin);
+            this.Controls.Add(this.label_balance);
+            this.Controls.Add(this.button_deposit);
+            this.Controls.Add(this.textBox_deposit);
+            this.Controls.Add(this.label_depositPrompt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_depositPrompt;
+        private System.Windows.Forms.TextBox textBox_deposit;
+        private System.Windows.Forms.Button button_deposit;
+        private System.Windows.Forms.Label label_balance;
+        private System.Windows.Forms.Label label_lastWin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label_betPrompt;
+        private System.Windows.Forms.ComboBox comboBox_bet;
+        private System.Windows.Forms.Label label_totalSpins;
+        private System.Windows.Forms.Label label_winCount;
+        private System.Windows.Forms.Label label_winRate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
